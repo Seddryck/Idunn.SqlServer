e417a6d4dcbf9a6b44556c96e4212d8e5414cac0
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-04-22 08:50:41 +0200

    Add parsers for xml files

---
+++ IdunnSql.Core/Parser/XmlParser/Parser.cs
@@ -0,0 +1,33 @@
+﻿using IdunnSql.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace IdunnSql.Core.Parser.XmlParser
+{
+    public class Parser
+    {
+        public Principal Parse(Stream stream)
+        {
+            using (var streamReader = new StreamReader(stream))
+            {
+                var xmlReaderFactory = new XmlReaderFactory();
+                using (var xmlReader = xmlReaderFactory.Instantiate(streamReader))
+                {
+                    var xmlDoc = new XmlDocument();
+                    xmlDoc.Load(xmlReader);
+
+                    var root = xmlDoc.FirstChild.NextSibling;
+
+                    var principalParser = new PrincipalParser();
+                    var principal = principalParser.Parse(root);
+                    return principal;
+                }
+            }
+        }
+    }
+}
---
+++ IdunnSql.Core/Parser/XmlParser/PrincipalParser.cs
@@ -0,0 +1,31 @@
+﻿using IdunnSql.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace IdunnSql.Core.Parser.XmlParser
+{
+    class PrincipalParser
+    {
+        public Principal Parse(XmlNode node)
+        {
+            if (node.Name != "principal")
+                throw new ArgumentException();
+
+            
+
+            var databases = new List<Database>();
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                var databaseParser = new DatabaseParser();
+                databases.Add(databaseParser.Parse(child));
+            }
+            var principal = new Principal(databases);
+            return principal;
+
+        }
+    }
+}
