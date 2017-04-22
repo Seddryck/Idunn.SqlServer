f2234ccced5a7252f8d5a287fec45c3032b91167
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-04-22 08:49:25 +0200

    Create model for main classes

---
+++ IdunnSql.Core/Model/ModelFactory.cs
@@ -0,0 +1,36 @@
+﻿using IdunnSql.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IdunnSql.Core
+{
+    class ModelFactory
+    {
+        public Principal Instantiate(string filename)
+        {
+            if (File.Exists(filename))
+                throw new ArgumentException(string.Format("No file has been found at the location '{0}'.", filename));
+
+            using (var stream = File.OpenRead(filename))
+            {
+                if (Path.GetExtension(filename) == ".xml")
+                    return InstantiateFromXml(stream);
+                else
+                    throw new NotImplementedException();
+            }
+
+        }
+
+        protected Principal InstantiateFromXml(Stream stream)
+        {
+            var parser = new Parser.XmlParser.Parser();
+            return parser.Parse(stream);
+        }
+    }
+}
---
+++ IdunnSql.Core/Model/Principal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdunnSql.Core.Model
+{
+    public class Principal
+    {
+        public Principal(List<Database> databases)
+        {
+            Databases = databases;
+        }
+
+        public string Name { get; }
+        public IReadOnlyCollection<Database> Databases { get; protected set; }
+
+        
+    }
+}
