8841e0f7c64b2ed7a17f6ca65382a421ed429fbd
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-05-03 00:55:48 +0200

    Add the -pre suffix if not from master

--- package-Idunn.SqlServer.ps1
+++ package-Idunn.SqlServer.ps1
@@ -1,6 +1,6 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition)
 $lib = "$root\.package\lib\45\"
-If (Test-Path $lib)
+if (Test-Path $lib)
 {
 	Remove-Item $lib -recurse
 }
@@ -8,11 +8,16 @@ new-item -Path $lib -ItemType directory
 new-item -Path $root\.nupkg -ItemType directory -force
 Copy-Item $root\Idunn.SqlServer.Console\bin\Debug\* $lib
 
-Write-Host "Setting .nuspec version tag to $env:GitVersion_NuGetVersion"
+$version = $env:GitVersion_NuGetVersion
+if ($env:APPVEYOR_REPO_BRANCH -ne "master")
+{
+    $version += '-pre'
+}
+Write-Host "Setting .nuspec version tag to $version"
 
 $content = (Get-Content $root\Idunn.SqlServer.nuspec -Encoding UTF8) 
-$content = $content -replace '\$version\$',$env:GitVersion_NuGetVersion
+$content = $content -replace '\$version\$',$version
 
 $content | Out-File $root\.package\Idunn.SqlServer.compiled.nuspec -Encoding UTF8
 
-& NuGet.exe pack $root\.package\Idunn.SqlServer.compiled.nuspec -Version $env:GitVersion_NuGetVersion -OutputDirectory $root\.nupkg
+& NuGet.exe pack $root\.package\Idunn.SqlServer.compiled.nuspec -Version $version -OutputDirectory $root\.nupkg
