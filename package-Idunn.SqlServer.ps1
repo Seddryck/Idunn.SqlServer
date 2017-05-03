337012e6af00c940cce0e331e5b07036c44d7e20
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-05-03 23:49:38 +0200

    Fix the nuget packager

--- package-Idunn.SqlServer.ps1
+++ package-Idunn.SqlServer.ps1
@@ -9,10 +9,6 @@ new-item -Path $root\.nupkg -ItemType directory -force
 Copy-Item $root\Idunn.SqlServer.Console\bin\Debug\* $lib
 
 $version = $env:GitVersion_NuGetVersion
-if ($env:APPVEYOR_REPO_BRANCH -ne "master" -and $env:APPVEYOR_REPO_TAG)
-{
-    $version += '-pre'
-}
 Write-Host "Setting .nuspec version tag to $version"
 
 $content = (Get-Content $root\Idunn.SqlServer.nuspec -Encoding UTF8) 
@@ -21,3 +17,4 @@ $content = $content -replace '\$version\$',$version
 $content | Out-File $root\.package\Idunn.SqlServer.compiled.nuspec -Encoding UTF8
 
 & NuGet.exe pack $root\.package\Idunn.SqlServer.compiled.nuspec -Version $version -OutputDirectory $root\.nupkg
+
